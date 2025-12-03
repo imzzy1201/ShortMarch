309966e96d0547573662ea35c8d38e77508d6199
imzzy1201 <imzzy1201@local.host>
2025-12-04 01:53:27 +0800

    Register buffers

--- src/shaders/shader.hlsl
+++ src/shaders/shader.hlsl
@@ -14,6 +14,49 @@ struct HoverInfo {
 	int hovered_entity_id;
 };
 
+struct InstanceInfo {
+    uint vertex_offset;
+    uint index_offset;
+    uint has_normal;
+    uint normal_offset;
+    uint has_texcoord;
+    uint texcoord_offset;
+    uint has_tangent;
+    uint tangent_offset;
+};
+
+struct PointLight {
+    float3 position;
+    float intensity;
+    float3 color;
+    float _pad;
+};
+
+struct AreaLight {
+    float3 position;
+    float intensity;
+    float3 color;
+    float _pad0;
+    float3 u;
+    float _pad1;
+    float3 v;
+    float _pad2;
+};
+
+struct SunLight {
+    float3 direction;
+    float intensity;
+    float3 color;
+    float _pad;
+};
+
+struct SceneInfo {
+    uint num_point_lights;
+    uint num_area_lights;
+    uint num_sun_lights;
+    uint _pad;
+};
+
 RaytracingAccelerationStructure as : register(t0, space0);
 RWTexture2D<float4> output : register(u0, space1);
 ConstantBuffer<CameraInfo> camera_info : register(b0, space2);
@@ -22,6 +65,16 @@ ConstantBuffer<HoverInfo> hover_info : register(b0, space4);
 RWTexture2D<int> entity_id_output : register(u0, space5);
 RWTexture2D<float4> accumulated_color : register(u0, space6);
 RWTexture2D<int> accumulated_samples : register(u0, space7);
+StructuredBuffer<PointLight> point_lights : register(t0, space8);
+StructuredBuffer<AreaLight> area_lights : register(t0, space9);
+StructuredBuffer<SunLight> sun_lights : register(t0, space10);
+ConstantBuffer<SceneInfo> scene_info : register(b0, space11);
+StructuredBuffer<InstanceInfo> instance_infos : register(t0, space12);
+StructuredBuffer<float3> vertices : register(t0, space13);
+StructuredBuffer<uint> indices : register(t0, space14);
+StructuredBuffer<float3> normals : register(t0, space15);
+StructuredBuffer<float2> texcoords : register(t0, space16);
+StructuredBuffer<float3> tangents : register(t0, space17);
 
 struct RayPayload {
 	float3 color;
